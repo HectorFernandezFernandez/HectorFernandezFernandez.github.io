4be8157f8469dd67fc887904f7b1aa5eb7d76d7b
HectorFernandezFernandez <h.fernandezmf@gmail.com>
2024-05-25 12:43:21 -0400

    arreglo como django

--- app/views.py
+++ app/views.py
@@ -5,3 +5,21 @@ from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def fashion(request):
+    return render(request, 'fashion.html')
+
+def photography(request):
+    return render(request, 'photography.html')
+
+def travel(request):
+    return render(request, 'travel.html')
+
+def single(request):
+    return render(request, 'single.html')
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
 from django.conf.urls.static import static
+from django.conf import settings
 from app import views
-from portfolio import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
+    path('fashion/', views.fashion, name='fashion'),
+    path('about/', views.about, name='about'),
+    path('photography/', views.photography, name='photography'),
+    path('travel/', views.travel, name='travel'),
+    path('single/', views.single, name='single'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
