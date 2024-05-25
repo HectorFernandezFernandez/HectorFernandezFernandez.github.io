af7e97c5cc76920a6d57a57bb1a30dc7b65f54d9
HectorFernandezFernandez <h.fernandezmf@gmail.com>
2024-05-25 11:06:58 -0400

    Pagina base

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
